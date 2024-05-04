880f896a28ff1592b12cf787cb9c190096ec629f
Francesc Vila <francescvilasubias@gmail.com>
2024-05-04 10:38:40 +0200

    fix: validate body

--- server/api/search/index.post.ts
+++ server/api/search/index.post.ts
@@ -1,14 +1,18 @@
+import * as v from 'valibot';
 import { createClient } from 'edgedb';
 import { getSimilarFunctions } from '~/dbschema/queries';
 
+const bodySchema = v.object({
+  query: v.array(v.number()),
+});
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+  const { query } = await readValidatedBody(event, (data) => v.parse(bodySchema, data));
 
   const client = createClient();
-  const result = await getSimilarFunctions(client, { query: await JSON.parse(body) });
+  const result = await getSimilarFunctions(client, { query });
 
   return {
-    status: 200,
-    body: result,
+    result,
   };
 });
