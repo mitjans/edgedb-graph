b6996bb85a183b6466dc57149ccf49d5254602f5
Carles Mitjans <carles.mitjans.coma@gmail.com>
2024-05-06 09:23:05 +0200

    chore: setup tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', '@nuxt/eslint'],
+  modules: ['@vueuse/nuxt', '@nuxt/eslint', '@nuxt/fonts', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
     edgedb: {
       authBaseUrl: '',
