71ba1f8d3bf6e0f74f555f99246cacd33e10fa0d
Carles Mitjans <carles.mitjans.coma@gmail.com>
2024-05-26 16:07:54 +0200

    fix: scrollbar css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['nuxt-icon', '@vueuse/nuxt', '@nuxt/eslint', '@nuxt/fonts', '@nuxtjs/tailwindcss'],
   vue: { propsDestructure: true },
+  css: ['assets/css/main.css'],
   runtimeConfig: {
     edgedb: {
       authBaseUrl: '',
